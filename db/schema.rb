d2fc00e6fdf1e4d3b9987e4bc9e0ba3581b4dbd7
Theingi Lin <102965090+theingi96@users.noreply.github.com>
2025-11-22 10:34:57 +0900

    error_database_name

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2025_11_21_234725) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
